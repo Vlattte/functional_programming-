6a35d443d818d880ce86f158b99ae97f975a1ec3
vlad_break47 <vladislav_smirnov01@mail.ru>
2024-03-04 15:55:59 +0300

    lab2: вывод починен, дебаг комментарии стерты, все работает

--- lab2/quicksort.scm
+++ lab2/quicksort.scm
@@ -1,4 +1,21 @@
-(define data (list 1 0 7 2 3 4 8 9 5 6))
+(define data (list 13 0 7 2 -33 4 8 91 5 6))
+
+(define concat_lists
+    (lambda (l1 l2)
+        (let loop ((l1 l1) (l2 l2))
+            (if (null? l1) l2
+                (begin
+                    ; пока в l1 больше одного элемента, обрезаем l1 и передаем дальше
+                    ; на расскрутке рекурсии приписываем элементы из l1 в l2 начиная с конца
+                    (if (not (null? (cdr l1)) )    ; когда остался один элемент                
+                        (set! l2 (loop (cdr l1) l2))
+                    )
+                    (cons (car l1) l2) 
+                )
+            )        
+        )
+    )    
+)
 
 (define quicksort       
     (lambda (l)
@@ -9,36 +26,23 @@
                 (define high '() )      ; больше опорного элемента
                 
                 ; тут делим по массивам low и high 
-                (if (null? (cdr l)) 
-                    (begin
-                        (format #t "EMPTY\n")
-                        l
-                    )  
+                (if (null? (cdr l)) l ; если в массиве один элемент, не сортируем
                     (begin             
                         (set! pivot (car l))
                         (let sort_loop ((l l) (pivot pivot) (low low) (high high))
-                            (if (null? l)       ; если больше нет элементов, то все
+                            (if (null? l)       ; элементы кончились => сортируем под массивы
                                 (begin 
-                                    (format #t "aaaaaaaa = ~a\n" pivot)
-
                                     ; если low не пустой (в high всегда хотя бы сам элемент лежит)
                                     (if (not (null? low) )
                                         (set! low (sort low))
                                     )                                    
-                                    (set! high (sort high))
-                                    (cons low high)
+                                    (set! high (sort high))  
+                                    (concat_lists low high) ; объединяем списки
                                 )
-                                (begin         
-                                    (format #t "pivot = ~a\n" pivot)               
+                                (begin               
                                     (if (> pivot (car l)) 
-                                        (begin 
-                                            (set! low (cons (car l) low ))
-                                            (format #t "low = ~a\n" low)                                             
-                                        )
-                                        (begin 
-                                            (set! high (cons (car l) high ) )
-                                            (format #t "\thigh = ~a\n" high)                                 
-                                        )
+                                        (set! low (cons (car l) low ))                                           
+                                        (set! high (cons (car l) high ) )
                                     )
                                     (sort_loop (cdr l) pivot low high)             
                                 )   
@@ -51,5 +55,5 @@
     )
 )
 
-; (format #t "~s\n" (list-ref data 1) )
 (format #t "Quicksort: ~a\n" (quicksort data) )
+;(format #t "Quicksort: ~a\n" (concat_lists data1 data2))
