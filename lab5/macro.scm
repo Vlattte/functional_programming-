c0d4e97dc5687347e13eb64ab3b33e674945b376
vlad_break47 <vladislav_smirnov01@mail.ru>
2024-04-23 16:45:14 +0300

    lab5: memoize, define-syntax, syntax-rules

---
+++ lab5/macro.scm
@@ -0,0 +1,47 @@
+(define-syntax memoize
+  (syntax-rules ()
+    ( 
+        (memoize (define func (lambda (args ...) body ...) ) )
+        (begin 
+            (define real_func (lambda (args ...) body ...))  
+            (define func
+            (let ( (cache '()) )
+                (lambda (args ...)
+                (if (eq? (assoc (list args ...) cache) #f)
+                    (begin
+                    (display "Calculating function with args ")
+                    (display (list args ...))
+                    (newline) 
+                    (set! cache (cons (list (list args ...) (real_func args ...)) cache))
+                    )
+                    '()
+                )
+                (cadr (assoc (list args ...) cache))
+                )
+            )
+            )
+        )
+    )
+  )
+)
+
+; decorating
+(memoize 
+(define fib
+    (lambda (n i)
+        (if (or (= n 1)
+                (= n 2)
+            ) 1 ; true
+            (+ (fib (- n 1) i)
+                (fib (- n 2) i)
+                i
+            ) ; false
+        )
+    )
+)
+)
+
+(display (fib 10 1))
+(newline) 
+(display (fib 10 1))
+(newline) 
